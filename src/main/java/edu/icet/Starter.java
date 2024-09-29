8e6e45b61ae7cd67fd481648e6e5965448347fda
XTshadowX <vinuthsriarampath@outlook.com>
2024-09-29 20:47:30 +0530

    Added Project Structure With Temp Classes and Interfaces And Also Added MOST OF THE EXTERNAL LIBRARIES TO THE POM.XML FILE.Also Added the hibernate.cfg.xml file with its HibernateUtil.java class

---
+++ src/main/java/edu/icet/Starter.java
@@ -0,0 +1,14 @@
+package edu.icet;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
