2c2cd83774d61754e0c8727e05001387695d9316
XTshadowX <vinuthsriarampath@outlook.com>
2024-10-08 20:00:30 +0530

    Added employee change password user interfaces

---
+++ src/main/java/controller/EmployeeChangePasswordFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EmployeeChangePasswordFormController {
+
+    @FXML
+    private JFXPasswordField pfConfirmPassword;
+
+    @FXML
+    private JFXPasswordField pfPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
